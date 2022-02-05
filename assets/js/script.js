a79d80a41889e901f5d504b1391a0109a3950159
gogogabe9000 <gabe.email1032@yahoo.com>
2022-02-05 11:50:04 -0500

    made coin flip randomly when flip button is pressed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,27 @@
 let numberOfHeadRolls = 0
 let numberOfTailRolls = 0
 const flipBtn = document.querySelector('#flip')
+const pennyImg = document.querySelector('#coinImage')
 
 
 document.addEventListener('DOMContentLoaded', function () {
 
     // TODO: Add event listener and handler for flip and clear buttons
 
-
-    
     // Flip Button Click Handler
-        // TODO: Determine flip outcome
+    flipBtn.addEventListener('click', function(){
+        let flippedHeads = Math.random() < 0.5
+
+        
         // TODO: Update image and status message in the DOM
+        if(flippedHeads){
+            pennyImg.setAttribute('src', 'assets/images/penny-heads.jpg')
+        } else {
+            pennyImg.setAttribute('src', 'assets/images/penny-tails.jpg')
+        }
+    })
+    
+        // TODO: Determine flip outcome
 
         // Update the scoreboard
         // TODO: Calculate the total number of rolls/flips
